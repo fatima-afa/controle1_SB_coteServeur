04e969fc2f228a174adb3e1e30cc50a651161bb8
fatima-afa <afafatima12@gmail.com>
2023-04-17 10:00:14 +0200

    first commit

---
+++ controleSpringBoot/src/main/java/fatima/afa/fatimaafa/ControleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package fatima.afa.fatimaafa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleSpringBootApplication.class, args);
+    }
+
+}
---
+++ controleSpringBoot/src/test/java/fatima/afa/fatimaafa/ControleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fatima.afa.fatimaafa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
